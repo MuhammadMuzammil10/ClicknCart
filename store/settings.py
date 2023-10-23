724a5608fd60dff843214975d1bf8ef2501e84e0
MuhammadMuzammil10 <141768444+MuhammadMuzammil10@users.noreply.github.com>
2023-10-23 23:08:34 +0500

    media root

--- store/settings.py
+++ store/settings.py
@@ -113,7 +113,9 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'app\static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+# MEDIA_ROOT = BASE_DIR / 'media'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'media')
+
 
 WSGI_APPLICATION = 'store.wsgi.application'
 
