3eeee59ee8db8a4dc3f8612f098313cdaf95ef24
Muhammad Muzammil <zmuzammil74@gmail.com>
2024-05-21 20:39:12 +0500

    update urls.py

--- store/urls.py
+++ store/urls.py
@@ -15,9 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include 
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-
 # from django.conf.urls import url
 from app import views
 
@@ -26,5 +26,5 @@ urlpatterns = [
     path('accounts/', include('allauth.urls')),
     path('',include('app.urls')),
 ]
-
 urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
