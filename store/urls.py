9882fa62ad6d76935231e9161c38429ccfb0682b
Muhammad Muzammil <zmuzammil74@gmail.com>
2024-05-21 21:40:33 +0500

    update settings

--- store/urls.py
+++ store/urls.py
@@ -25,6 +25,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('',include('app.urls')),
-]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-urlpatterns+=staticfiles_urlpatterns()
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
