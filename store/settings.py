48f68d99e90d06f242d683bdb36aa9b75c54a437
Muhammad Muzammil <zmuzammil74@gmail.com>
2024-05-21 20:54:18 +0500

    update settings.py

--- store/settings.py
+++ store/settings.py
@@ -126,7 +126,7 @@ if ENVIRONMENT == 'production' or POSTGRES_LOCALLY == True:
     DATABASES['default'] = dj_database_url.parse(env('DATABASE_URL'))
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = 'media/'
